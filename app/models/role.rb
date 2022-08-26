4d971ef1f48a25f12788274cac1e1ab6087c48f6
Laura Berge <lauraeberge@gmail.com>
2022-08-26 12:25:39 -0500

    add message if no actor is hired

--- app/models/role.rb
+++ app/models/role.rb
@@ -10,10 +10,10 @@ class Role < ActiveRecord::Base
     end
 
     def lead
-        auditions.find_by(hired: true)
+        auditions.find_by(hired: true) || 'no actor has been hired for this role'
     end
 
     def understudy
-        auditions.where(hired: true).second
+        auditions.where(hired: true).second || 'no actor has been hired for this role'
     end
 end
